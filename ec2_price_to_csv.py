10538b7391be0731ebc11f2223da58ad7929bd10
Nicholas Guthrie <nicholas.guthrie4@gmail.com>
2020-10-03 18:27:52 -0400

    committing patch of changes. Not sure why repos are currently out of sync

--- ec2_price_to_csv.py
+++ ec2_price_to_csv.py
@@ -5,6 +5,7 @@ from pkg_resources import resource_filename
 import pprint
 from datetime import datetime
 import pandas as pd
+import datetime
 
 def test_credentials():
     s3 = boto3.resource('s3', aws_access_key_id = aws_creds.access_key,
@@ -39,6 +40,7 @@ def get_price(region, instance, os):
     id2 = list(od[id1]['priceDimensions'])[0]
     return od[id1]['priceDimensions'][id2]['pricePerUnit']['USD']
 
+
 # Translate region code to region name
 # Cody-pasta: not sure why this is necessary over passing a string
 def get_region_name(region_code):
@@ -54,7 +56,13 @@ def get_region_name(region_code):
 
 # Use AWS Pricing API at US-East-1
 client = boto3.client('pricing', region_name='us-east-1', aws_access_key_id = aws_creds.access_key,
-                    aws_secret_access_key = aws_creds.secrete_key)
+                      aws_secret_access_key = aws_creds.secrete_key)
+
+
+# Get current price for a given instance, region and os
+# price = get_price(get_region_name('us-east-1'), 't2.micro', 'Linux')
+# print(price)
+
 '''
 # list containing ec2 instances, copied from webpage
 with open("ec2_to_fetch_list.txt", 'r') as file:
@@ -79,10 +87,6 @@ name = 'ec2prices.csv'
 ec2_price_df.to_csv(name, index=False)
 '''
 
-# Get current price for a given instance, region and os
-price = get_price(get_region_name('us-east-1'), 't2.micro', 'Linux')
-print(price)
-
 
 def get_all_instance_names():
 
@@ -104,3 +108,86 @@ def get_all_instance_names():
         print(ec2_type)
 
 # get_all_instance_names()
+
+'''
+response = client.describe_spot_price_history(
+    Filters=[{
+        'Name': 'availability-zone',
+        'Values': ['us-east-1a', 'us-east-1c', 'us-east-1d']
+        },
+        {
+            'Name': 'product-description',
+            'Values': ['Linux/UNIX (Amazon VPC)']
+        }],
+    InstanceTypes=['r5.2xlarge'],
+    EndTime=datetime.now(),
+    StartTime=datetime.now()
+)
+'''
+
+client = boto3.client('ec2', region_name='us-east-1')
+
+def get_spot_prices(instance_type):
+    # (typically) returns the most recent spot price for each availability zone
+
+    prices = client.describe_spot_price_history(InstanceTypes=[instance_type], MaxResults=20,
+                                                ProductDescriptions=['Linux/UNIX (Amazon VPC)']
+                                                )
+    # AvailabilityZone='us-east-1a'
+
+
+    # dict with AZ's as keys and prices as values
+    AZ_price_dict = {}
+    for result in prices['SpotPriceHistory']:
+        price = result['SpotPrice']
+        AZ = result['AvailabilityZone']
+        # want to keep adding zones without overriding the most recent prices
+        if not AZ in AZ_price_dict.keys():
+            AZ_price_dict[AZ] = price
+
+    pprint.pprint(prices['SpotPriceHistory'])
+    # print(AZ_price_dict)
+    return(AZ_price_dict)
+
+
+instance_type = 't2.micro'
+
+AZ_price_dict = get_spot_prices(instance_type)
+print(AZ_price_dict)
+
+def get_best_AZ(AZ_price_dict):
+    # get the best price from the dict
+    best_price = min(AZ_price_dict.values())
+    for AZ, price in AZ_price_dict.items():
+        if price == best_price:
+            return AZ, price
+
+cheapest_AZ, price = get_best_AZ(AZ_price_dict)
+
+print("Cheapest AZ:", cheapest_AZ)
+print("Lowest price:", float(price) + 0.005)
+
+
+def create_spot_instance(instance_type, cheapest_AZ):
+    response = client.request_spot_instances(
+        DryRun=False,
+        SpotPrice= str(float(price) + 0.005),
+        ClientToken='string1',
+        InstanceCount=1,
+        Type='one-time',
+        LaunchSpecification={
+            'ImageId': 'ami-0947d2ba12ee1ff75',
+            'KeyName': 'FirstKey',
+            'SecurityGroups': ['flask_group'],
+            'InstanceType': instance_type,
+            'Placement': {
+                'AvailabilityZone': cheapest_AZ,
+            },
+            'SecurityGroupIds': [
+                'sg-037901ad326336e81',
+            ]
+        }
+    )
+    print(response)
+
+create_spot_instance(instance_type, cheapest_AZ)
