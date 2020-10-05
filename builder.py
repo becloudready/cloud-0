2bb9b0e8e254cad6e3eb9978b776956d1fd4e3e6
Nicholas Guthrie <nicholas.guthrie4@gmail.com>
2020-10-04 22:09:36 -0400

    minor updates to variable names

--- builder.py
+++ builder.py
@@ -13,19 +13,23 @@ class Builder():
         self.price = price
         self.client = client
 
-    def create_spot_instance(self, instance_type, cheapest_AZ, new=True):
+    def create_spot_instance(self, instance_type, cheapest_AZ, new_instance=True, *args, **kwargs):
         """Create a spot request that, if accepted, will create a ec2 instance for that price."""
 
         DryRun = False
-        SpotPrice = str(float(self.price) + 0.005)
+        SpotPrice = self.price
         client = self.client
         InstanceCount=1
         Type='one-time'
+        security_group = 'flask_group'
+        KeyName = 'FirstKey'
+        ImdageId = 'ami-0947d2ba12ee1ff75'
+        SecurityGroupIds = 'sg-037901ad326336e81'
 
         # request_spot_instances is idempotent on the string token
         # for a new request, a new string is needed
         # for a repeated request that that shouldn't be duplicated, should use the same token
-        if new:
+        if new_instance:
             ClientToken = self.str_generator()
         else:
             ClientToken = 'default'
@@ -37,15 +41,15 @@ class Builder():
             InstanceCount=InstanceCount,
             Type=Type,
             LaunchSpecification={
-                'ImageId': 'ami-0947d2ba12ee1ff75',
-                'KeyName': 'FirstKey',
-                'SecurityGroups': ['flask_group'],
+                'ImageId': ImdageId,
+                'KeyName': KeyName,
+                'SecurityGroups': [security_group],
                 'InstanceType': instance_type,
                 'Placement': {
                     'AvailabilityZone': cheapest_AZ,
                 },
                 'SecurityGroupIds': [
-                    'sg-037901ad326336e81',
+                    SecurityGroupIds,
                 ]
             }
         )
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-
 import pricer
 import builder
 
-pricer.test_found()
+
+if __name__ == "__main__":
+
+    # create test pricer
+    pricer1 = pricer.Pricer('ec2', 'us-east-1', 't2.micro')
+    prices_response = pricer1.get_spot_prices()
+    AZ_price_dict = pricer1.format_price_data(prices_response)
+    AZ, price = pricer1.get_best_price(AZ_price_dict)
+
+    client = pricer1.return_client()
+
+    # create test instance
+    builder1 = builder.Builder(price, client)
+    instance_type = 't2.micro'
+    builder1.create_spot_instance(instance_type, AZ)
+
 
 
 
